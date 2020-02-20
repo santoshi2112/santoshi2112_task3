25f7feaba6cc458027041cb72a59c44c4d1ec60b
santoshi2112 <60030154+santoshi2112@users.noreply.github.com>
2020-02-20 20:11:12 +0530

    Add files via upload
    yagni commit

---
+++ Task3_yagni/src/main/java/com/epam/Task3_yagni/yagni.java
@@ -0,0 +1,20 @@
+package com.epam.Task3_yagni;
+
+public class yagni {
+	public static void main(String args[]) {
+		class1 obj=new class1();
+		obj.printstatus();
+	}
+
+	}
+	class class1{
+		void printstatus() {
+			System.out.print("this method is needed because we are using in yagni class");
+		}
+		/***
+		 *commenting all not needed methods
+		  void printgreeting(){
+		  System.out.println("hello);
+		 */
+
+}
